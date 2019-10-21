ec0fc8a2455005ef5f9370d720ec4deee8f8b471
Ollie <ospillett@hotmail.com>
2019-10-21 15:41:03 +0100

    initial kivy commit

---
+++ examples/Kivy_Screens/main.py
@@ -0,0 +1,27 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.unload_file('robot.kv')
+Builder.load_file('robot.kv')
+
+class EyeScreen(Screen):
+    pass
+
+class MenuScreen(Screen):
+    pass
+
+sm = ScreenManager()
+sm.add_widget(EyeScreen(name='eyes'))
+sm.add_widget(MenuScreen(name='menus'))
+
+sm.current = 'menus'
+
+class RobotApp(App):
+    def build(self):
+        return sm
+
+if __name__ == '__main__':
+    RobotApp().run()
+
