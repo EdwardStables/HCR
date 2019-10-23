a5fd83ed8322bf50aa22906b7f5fc7d9d10e83f2
Ollie <ospillett@hotmail.com>
2019-10-23 15:48:44 +0100

    Image with text input to control mood property and colours invert on mood <1

--- examples/Kivy_Screens/main.py
+++ examples/Kivy_Screens/main.py
@@ -2,21 +2,47 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.image import Image
 from kivy.lang import Builder
+from kivy.event import EventDispatcher
+from kivy.properties import NumericProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 Builder.load_file('robot.kv')
 
 class EyeScreen(Screen):
-    pass
+    mood = NumericProperty(1.0)
+
+    def update_mood(self):
+        value = self.ids['moodinput']
+        self.mood = value.text
+        print 'changing mood'
+    
+    def switch_image(self, *args):
+        eyeimage = self.ids['screenimage']
+        print 'hello' + str(eyeimage.source)
+        if self.mood < 1.0:
+            eyeimage.source = 'eyestest2.png'
+        else:
+            eyeimage.source = 'eyestest.png'
 
 class MenuScreen(Screen):
     pass
 
+def moodcallback(instance,value):
+    print('mood changed to',value)
+    instance.switch_image()
+
+
 sm = ScreenManager()
-sm.add_widget(EyeScreen(name='eyes'))
-sm.add_widget(MenuScreen(name='menus'))
+eyescreen = EyeScreen(name='eyes')
+menuscreen = MenuScreen(name='menus')
+
+eyescreen.bind(mood=moodcallback)
+
+sm.add_widget(eyescreen)
+sm.add_widget(menuscreen)
 
 #sm.current = 'menus'
+
 
 class RobotApp(App):
     def build(self):
