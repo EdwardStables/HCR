d1e8015d209ae72fdbb3d8abf77013e56b1dbfdc
Ollie <ospillett@hotmail.com>
2019-10-28 15:36:58 +0000

    Ported kivy interface to Python 3.7 and added basic kivy animations instead of static images

--- experiments/Kivy_Screens/main.py
+++ experiments/Kivy_Screens/main.py
@@ -1,33 +1,43 @@
 from kivy.app import App
+
 from kivy.uix.widget import Widget
 from kivy.uix.image import Image
-from kivy.lang import Builder
-from kivy.event import EventDispatcher
-from kivy.properties import NumericProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
+
+from kivy.graphics import Rectangle, Color, Ellipse
+from kivy.animation import Animation
+from kivy.lang import Builder
+from kivy.properties import NumericProperty
 
 Builder.load_file('robot.kv')
 
 class EyeScreen(Screen):
-    mood = NumericProperty(1.0)
+    mood = NumericProperty(1.0) #parameter for updating mood displayed by robot
 
-    def update_mood(self):
+    def update_mood(self):      #callback for whenever the text box is updated
         value = self.ids['moodinput']
         self.mood = value.text
-        print 'changing mood'
+        print('changing mood')
     
-    def switch_image(self, *args):
-        eyeimage = self.ids['screenimage']
-        print 'hello' + str(eyeimage.source)
+    def switch_image(self, *args):  #whenever the mood is changed update the screen accordingly
+        eyeimage = self.ids['lefteye']
         if self.mood < 1.0:
-            eyeimage.source = 'eyestest2.png'
+            anim = Animation(x=300, y=100)
+            anim.start(eyeimage)
         else:
-            eyeimage.source = 'eyestest.png'
+            anim = Animation(x=100, y=100)
+            anim.start(eyeimage)
 
 class MenuScreen(Screen):
     pass
 
-def moodcallback(instance,value):
+class EyeImage(Widget):
+    pass
+
+class PupilImage(Widget):
+    pass
+
+def moodcallback(instance,value):   #callback for whenever the mood is updated
     print('mood changed to',value)
     instance.switch_image()
 
@@ -42,7 +52,7 @@ sm.add_widget(eyescreen)
 sm.add_widget(menuscreen)
 
 #sm.current = 'menus'
-
+#eyescreen.mood = 2.0
 
 class RobotApp(App):
     def build(self):
