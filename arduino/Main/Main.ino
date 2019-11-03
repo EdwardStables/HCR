5462fb65fd7cd147399f9004ea29b483e97b0dd2
Alex P <47080630+Xride20@users.noreply.github.com>
2019-11-03 17:07:26 +0000

    Added comments to code

--- arduino/Main/Main.ino
+++ arduino/Main/Main.ino
@@ -27,38 +27,45 @@ void setup() {
 
 void loop() {
   
+  // Setup JSON input, 1024 bytes
   StaticJsonDocument<1024> doc;
   String inData;
   //char json[48];
   int offset[2];
 
+  // Not tested but should read Serial data into a string
   /*while (Serial.available() > 0) {
     char received = Serial.read();
     inData += received;
   }
 
+  // Convert Serial data to a char Array
   inData.toCharArray(json, 48);*/
-  //char json[] = "{\"instr\":1, \"moves\":[[50, 50, 50, pi/4, pi/4, pi/4]]}";
+  // Test JSON variable
   char json[] = "{\"instr\":1,\"moves\":[[0,1,2,3,4,5],[6,7,8,9,10,11]]}";
   // Deserialize the JSON document
   DeserializationError error = deserializeJson(doc, json);
 
-  // Test if parsing succeeds.
+  // Test if parsing succeeds
   if (error) {
     Serial.print(F("deserializeJson() failed: "));
     Serial.println(error.c_str());
     return;
   }
 
+  // Simple function call to handle state change (function not written yet)
   //int state = doc["state"];
   //changeState(state);
   
+  // Switch statement for instruction type
   int instr = doc["instr"];
   switch (instr) {
 
+    // Case 1 is a movement pattern, eg dancing
     case 1:
     int arraySize = doc["moves"].size();
     float moves[arraySize][6];
+    // Loop to copy doc["moves"] array as it behaves strangely (data type cannot be inferred)
     for (int x = 0; x < arraySize; x++) {
       for (int y = 0; y < 6; y++) {
         moves[x][y] = doc["moves"][x][y];
@@ -67,10 +74,16 @@ void loop() {
     makeMoves(moves, arraySize);
     break;
 
+    // Case 2 is the offset instruction
     case 2:
-    offset[2] = doc["offset"];
+    float offset[2];
+    // Loop to copy doc["offset"] array as it behaves strangely (data type cannot be inferred)
+    for (int x = 0; x < 2; x++) {
+        moves[x] = doc["offset"][x];
+    }
     applyOffset(offset);
 
+    // Default case resets position
     default:
     reset();
     break;
@@ -79,12 +92,15 @@ void loop() {
 }
 
 void changeState(int state) {
-  
+  // Function to change LED to indicate current state (not written yet)
+  Serial.print("State changed to state ");
+  Serial.println(state);
 }
 
 void makeMoves(float moves[][6], int arraySize) {
   static Vector trans;
   static Vector rotat;
+  // Loops through moves Array and applies moves
   for (int i = 0; i < arraySize; i++) {
     trans.x = moves[i][0];
     trans.y = moves[i][1];
@@ -93,7 +109,9 @@ void makeMoves(float moves[][6], int arraySize) {
     rotat.y = moves[i][4];
     rotat.z = moves[i][5];
     
+    // Jack may need to alter the library so that this single instruction executes the movement at the correct speed
     Platform.applyTranslationAndRotation(trans, rotat);
+    // Checks if new instruction is available IE interrupts
     if (Serial.available() > 0) {
       loop();
     }
@@ -103,7 +121,7 @@ void makeMoves(float moves[][6], int arraySize) {
 
 void applyOffset(int offset[2]) {
   int tx, ty, tz, rx, ry, rz;
-  // convert offset to xyz xyz
+  // Offset needs converting to xyz xyz
   static Vector trans;
   static Vector rotat;
   trans.x = tx;
@@ -118,11 +136,12 @@ void applyOffset(int offset[2]) {
 void reset() {
   static Vector trans;
   static Vector rotat;
+  // These values need calibrating I think
   trans.x = 0;
   trans.y = 0;
   trans.z = 0;
-  rotat.x = 0;
-  rotat.y = 0;
-  rotat.z = 0;
+  rotat.x = pi / 4;
+  rotat.y = pi / 4;
+  rotat.z = pi / 4;
   Platform.applyTranslationAndRotation(trans, rotat);
 }
