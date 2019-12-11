cadfa2656ffafe94ee30ae2f0c77ae84b58c5786
EdwardStables <edward.stables1198@gmail.com>
2019-12-11 13:36:27 +0000

    added fan controller

--- experiments/fan/FanTest.py
+++ experiments/fan/FanTest.py
@@ -1,10 +1,53 @@
 import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT)
-print("FAN ON")
-GPIO.output(18, GPIO.HIGH)
-time.sleep(10)
-print("FAN OFF")
-GPIO.output(18, GPIO.LOW)
+from time import sleep
+import subprocess
+import sys
+
+ON_TEMP = 60
+OFF_TEMP = 55
+FAN_PIN = 12 
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(FAN_PIN, GPIO.OUT)
+
+def poll():
+    val = subprocess.getoutput("vcgencmd measure_temp")#, shell=True)
+    val = val.split('=')[-1]
+    val = val.split("'")[0]
+    return float(val)
+    
+def fan_on():
+    GPIO.output(FAN_PIN, GPIO.HIGH)
+
+def fan_off():
+    GPIO.output(FAN_PIN, GPIO.LOW)
+
+def main_loop(PRINT):
+    while True:
+
+        temp = poll()
+
+        if PRINT:
+            print(temp)
+
+        if temp > ON_TEMP:
+            fan_on()
+        elif temp < OFF_TEMP:
+            fan_off()
+
+        sleep(1)
+
+def argparse(arg):
+
+    if len(arg) > 1 and "print" ==  arg[1]:
+        return True
+    else:
+        return False
+    
+
+if __name__ == "__main__":
+    setup()
+    PRINT = argparse(sys.argv)
+    main_loop(PRINT)
